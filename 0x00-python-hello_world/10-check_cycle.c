a81223937bd5c9cba81d5b504d51e9eb357de60e
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2023-01-03 13:07:26 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,50 +1,69 @@
-#include <stdio.h>
-
+#include "lists.h"
 #include <stdlib.h>
 
+/**
+ * _realloc - Reallocates a memory block
+ * @ptr: The pointer to the previous memory block
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: The pointer to the new memory block otherwise NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int min_size = old_size < new_size ? old_size : new_size;
+	unsigned int i;
 
-
-typedef struct listint_s {
-
-  int n;
-
-  struct listint_s *next;
-
-} listint_t;
-
-
-
-int check_cycle(listint_t *list) {
-
-  listint_t *slow = list;
-
-  listint_t *fast = list;
-
-
-
-  while (slow && fast && fast->next) {
-
-    slow = slow->next;
-
-    fast = fast->next->next;
-
-    if (slow == fast) {
-
-      return 1;
-
-    }
-
-  }
-
-  return 0;
-
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL)
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		new_ptr = malloc(new_size);
+		if (new_ptr != NULL)
+		{
+			for (i = 0; i < min_size; i++)
+				*((char *)new_ptr + i) = *((char *)ptr + i);
+			free(ptr);
+			return (new_ptr);
+		}
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
 }
 
 
 
-int main() {
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list head
+ * Return: 0 if no, 1 if yes
+ */
 
-  // Test code goes here
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow = list;
 
-  return 0;
+	if (list == NULL)
+		return (0);
+
+	fast = list->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return (0);
 }

b124c807b63ab534b8a2e92d0e7bb3fb58e0a10d
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2023-02-06 16:02:44 -0800

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+''' Module: 1-my_list
+'''
 
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+class MyList(list):
+    ''' Represents a MyList
+    '''
+
+    def print_sorted(self):
+        '''
+        prints the list, but sorted
+        '''
+        print(sorted(self))

68d9763f75f086f5dd8e2855d68bab712e9c1021
Developer-in-Tech <princeakinola05@gmail.com>
2023-04-30 03:41:12 -0700

    .exe

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv[2];
-console.log(Number.isInteger(Number(arg))) ? `My number: ${parseInt(arg)}` : "Not a Number";
+console.log((/^\d+$.test(arg)) ? `My number: ${parseInt(arg)}` : "Not a Number");

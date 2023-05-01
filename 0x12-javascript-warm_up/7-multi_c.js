2dd85562a07df1e8f4828e54b6cd221bdc4ec7c3
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-01 05:51:29 -0700

    file7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const arg = procee.argv[2];
-
-if (isNaN(parseInt(arg))) {
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-  const num = Math.floor(parseInt(arg));
-  let output = '';
-
   for (let i = 0; i < num; i++) {
-    output += 'C is fun\n';
+    console.log('C is fun');
   }
-  console.log(output.trim());
 }

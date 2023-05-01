81876d2a6b9ecb488554335f5941ee158a6c90f8
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-01 05:18:46 -0700

    file 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = procee.argv[2];
+const num = parseInt(arg);
+
+if (isNaN(num)) [
+  console.log('Missing number of occurrences');
+} else {
+  let output = '';
+  for (let i = 0; i < num; i++) {
+    output += 'C is fun\n';
+  }
+  console.log(output);
+}

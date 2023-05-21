8b33e791deef3f36012259e7a5e80e37f78c43f0
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-21 08:35:12 +0100

    done

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 alphabet = ""
 
-for ascii_value in range (ord('a'), ord('z') +1):
+for ascii_value in range(ord('a'), ord('z') + 1):
     char = chr(ascii_value)
     alphabet += char
 

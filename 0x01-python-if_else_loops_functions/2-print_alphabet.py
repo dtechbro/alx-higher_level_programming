e6028fc122206a8a2c68f13ac21def76d499c3e0
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-21 09:05:48 +0100

    'try

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-alphabet = ""
 
-for ascii_value in range(ord('a'), ord('z') + 1):
-    char = chr(ascii_value)
-    alphabet += char
-
-output = "{}".format(alphabet)
-print(output, end='')
+output = ""
+for character in range(97, 123):
+    output = "{:c}".format(character)
+    print(output, end = "")
